84902a705412bd6a0ea842ab6e37c43ad478cb50
SimeonGetnetGesese <simeongetnet@gmail.com>
2021-10-12 19:22:17 -0700

    ANswer yo Mandatory Questions

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''task 8 module'''
+
+
+def class_to_json(obj):
+    '''returns dict discription with simple data structure'''
+    return (obj.__dict__)#!/usr/bin/python3
+'''task 8 module'''
+
+
+def class_to_json(obj):
+    '''returns dict discription with simple data structure'''
+    return (obj.__dict__)
